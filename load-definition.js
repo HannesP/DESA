3f0aab1e605375c0744fa5fd6e1884863ab11d7e
Hannes Petri <mail@hannesp.se>
2018-03-17 11:51:50 +0100

    Sanity

--- load-definition.js
+++ load-definition.js
@@ -2,7 +2,17 @@ const path = require('path');
 const yaml = require('js-yaml');
 const fs = require('fs');
 
+function isSane(definition) {
+	return true;
+}
+
 const defPath = path.join(__dirname, 'definition.yaml');
 const definition = yaml.load(fs.readFileSync(defPath));
+
+if (!isSane(definition)) {
+	throw 'Insane definition!';
+}
+
+definition.messages = {...definition.events, ...definition.commands};
 
 module.exports = definition;
